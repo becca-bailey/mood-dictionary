e345b0d7d77c0d732b95ea4328ed9ef38a195549
Becca Nelson <beccanelson88@gmail.com>
2016-04-09 14:53:16 -0500

    Add additional data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,19 +35,19 @@ depressed = sad.secondary_moods.create(name: "Depressed")
 ashamed = sad.secondary_moods.create(name: "Ashamed")
 guilty = sad.secondary_moods.create(name: "Guilty")
 
-mad.secondary_moods.create(name: "Hurt")
-mad.secondary_moods.create(name: "Hostile")
-mad.secondary_moods.create(name: "Angry")
-mad.secondary_moods.create(name: "Rage")
-mad.secondary_moods.create(name: "Hateful")
-mad.secondary_moods.create(name: "Critical")
+hurt = mad.secondary_moods.create(name: "Hurt")
+hostile = mad.secondary_moods.create(name: "Hostile")
+angry = mad.secondary_moods.create(name: "Angry")
+rage = mad.secondary_moods.create(name: "Rage")
+hateful = mad.secondary_moods.create(name: "Hateful")
+critical = mad.secondary_moods.create(name: "Critical")
 
-scared.secondary_moods.create(name: "Rejected")
-scared.secondary_moods.create(name: "Confused")
-scared.secondary_moods.create(name: "Helpless")
-scared.secondary_moods.create(name: "Submissive")
-scared.secondary_moods.create(name: "Insecure")
-scared.secondary_moods.create(name: "Anxious")
+rejected = scared.secondary_moods.create(name: "Rejected")
+confused = scared.secondary_moods.create(name: "Confused")
+helpless = scared.secondary_moods.create(name: "Helpless")
+submissive = scared.secondary_moods.create(name: "Submissive")
+insecure = scared.secondary_moods.create(name: "Insecure")
+anxious = scared.secondary_moods.create(name: "Anxious")
 
 # Scales
 
@@ -55,3 +55,23 @@ major = Scale.create(name: "C major scale", description: 'Happy; can be majestic
 
 major.primary_mood_id = loving.id
 major.secondary_mood_id = proud.id
+
+mixolydian = Scale.create(name: "C Mixolydian mode", description: "Bluesy, rock; can also be exotic/modal. Play over C7 chord. Same pitches as F major. Example: “Tomorrow Never Knows” by the Beatles.", i: "C", ii: "Dm", iii: "Edim", iv: "F", v: "Gm", vi: "Am", vii: "A#")
+
+mixolydian.primary_mood_id: energetic.id
+mixolydian.secondary_mood_id: proud.id
+
+lydian = Scale.create(name: "C Lydian mode", description: "Ethereal, dreamy, futuristic. Same pitches as G major. Example: “Possibly Maybe” by Björk (from the line “As much as I definitely enjoy solitude…”)", i: "C", ii: "Dm", iii: "Em", iv: "F#dim", v: "G", vi: "Am", vii: "A#")
+
+lydian.primary_mood_id: creative.id
+lydian.secondary_mood_id: intimate.id
+
+natural_minor = Scale.create(name: "C natural minor scale", description: "Sentimental, tragic. Same pitches as E flat major.", i: "Cm", ii: "Ddim", iii: "D#", iv: "Fm", v: "Gm", vi: "G#", vii: "A#")
+
+natural_minor.primary_mood_id: lonely.id
+natural_minor.secondary_mood_id: depressed.id
+
+harmonic_minor = Scale.create(name: "C harmonic minor scale", description: "Tragic, exotic, Middle Eastern.", i: "C", ii: "Ddim", iii: "D#aug", iv: "Fm", v: "G", vi: "G#", vii: "G#dim")
+
+harmonic_minor.primary_mood_id: hurt.id
+harmonic_minor.secondary_mood_id: rejected.id
